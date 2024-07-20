d5eb807db7bd10ad1fc29b263dbcb07d935a341e
gauravkus <gauravgzp114@gmail.com>
2024-07-20 19:07:42 +0530

    UPLOADED

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./component/About";
+import React, { useState } from "react";
+import Navbar from "./component/Navbar";
+import TextForm from "./component/TextForm";
+import Alert from "./component/Alert";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+
 
 function App() {
+  const [mode, setMode] = useState('light');   //Whether dark mode enabled or not.
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert = (message, type) => {
+
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark")
+      document.body.style.backgroundColor = "#193359";
+      showAlert("Dark mode has been enabled", "success")
+    }
+    else {
+      setMode("light")
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" About=" About Us" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+
+          <Switch>
+
+            <Route exact path="/about">
+              <About mode= {mode} />
+            </Route>
+
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading=" Try TextUtils - Word Counter , Character Counter , Remove extra spaces" mode={mode} />
+            </Route>
+
+          </Switch>
+
+        </div>
+      
+      </Router>
+
+
+
+    </>
   );
 }
 
